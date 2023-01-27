0a67a6a496a47aa0033f86aecafb9cb9ca546c29
Sinus44 <59234792+Sinus44@users.noreply.github.com>
2023-01-27 16:37:03 +0300

    Delete client/app/Core directory

--- client/app/Core/GUI/Frame.py
+++
@@ -1,9 +0,0 @@
-class Frame:
-    """Основа кадра и заливка фона"""
-    
-    def __init__(self, screen, style):
-        self.screen = screen
-        self.style = style
-    
-    def draw(self):
-        self.screen.fill(self.style["background"] + " ")
--- client/app/Core/GUI/Label.py
+++
@@ -1,7 +0,0 @@
-from Core.GUI.Element import Element
-
-class Label(Element):
-    """GUI - элемент текст"""
-    def draw(self):
-        if self.visible:
-            self.screen.text(self.text, self.x, self.y, wordPrefix = self.style["background"] + self.style["text"])
